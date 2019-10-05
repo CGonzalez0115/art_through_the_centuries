ca77746d71c56e54c731fc8b128bedb7cd1a4568
Christina Gonzalez <cmgamway@gmail.com>
2019-10-05 07:55:32 +0000

    art_through_the_centuries

--- lib/art_through_the_centuries.rb
+++ lib/art_through_the_centuries.rb
@@ -2,5 +2,10 @@ require "art_through_the_centuries/version"
 
 module ArtThroughTheCenturies
   class Error < StandardError; end
-  # Your code goes here...
+ require 'pry'
+ require 'Nokogiri'
+ require 'open-uri'
+ require_relative './art_through_the_centuries/lib/artist'
+ require_relative './art_through_the_centuries/centuries'
+ require_relative './art_through_the_centuries/works_of_art'
 end
