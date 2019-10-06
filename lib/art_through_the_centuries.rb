6df9de41338d239ce9996372b88c230ed7a1706a
Christina Gonzalez <cmgamway@gmail.com>
2019-10-06 04:00:51 +0000

    art_through_the_centuries.rb

--- lib/art_through_the_centuries.rb
+++ lib/art_through_the_centuries.rb
@@ -3,7 +3,7 @@ require "art_through_the_centuries/version"
 module ArtThroughTheCenturies
   class Error < StandardError; end
  require 'pry'
- require 'Nokogirl'
+ require 'nokogiri'
  require 'open-uri'
  require_relative './art_through_the_centuries/lib/artist'
  require_relative './art_through_the_centuries/centuries'
