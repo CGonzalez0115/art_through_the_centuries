e121e960be9fa3a718cffebae2f282e29eca00ce
Christina Gonzalez <cmgamway@gmail.com>
2019-10-13 23:23:01 +0000

    art_timeline_search.rb

--- lib/art_throughout_the_years/art_timeline_search.rb
+++ lib/art_throughout_the_years/art_timeline_search.rb
@@ -4,5 +4,25 @@ class ArtThroughoutTheYears :: ArtTimelineSearch
   YEAR = []
   TIME_PERIOD =[]
   
-
+  def call
+    puts "WELCOME TO YOUR ART HISTORY TIMELINE SEARCH!".indigo.bold
+    start
+  end
+  
+  def start
+    puts ""
+    puts "Please type and artiist's name, title of work, year, or time period.".navy_blue.bold
+    puts "(Please input your selection.)".navy_blue
+    input.gets.strip
+    
+    print_timeline(artwork)
+  end
+  
+  def print_timeline(artwork)
+    puts ""
+    puts "---------#{artwork.name} - #{artwork.title}--------"
+    puts "Artist Name:      #{artwork.name}"
+    puts "Title:            #{artwork.title}"
+    puts "Time Period:      #{artwork.time_period}"
+  end
 end
