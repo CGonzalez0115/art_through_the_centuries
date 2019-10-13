3dc3e42f19a7bf3722299cf13de4a8b8aad4ed3e
Christina Gonzalez <cmgamway@gmail.com>
2019-10-13 22:53:05 +0000

    art_throughout_the_years

--- lib/art_throughout_the_years/works_of_art.rb
+++ lib/art_throughout_the_years/works_of_art.rb
@@ -1,5 +1,15 @@
+require 'pry'
 class Works_of_Art
   
   attr_accessor :artist, :select_year, :title, :url, :description
   
+  def list_artists
+    binding.pry
+    input = gets.strip
+    if input == "list"
+      puts ""
+      artists = ArtThroughoutTheYears::Artist.all
+    end
+  end
+    
 end
