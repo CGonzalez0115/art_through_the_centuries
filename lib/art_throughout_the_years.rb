41d9755dcbdd925c1b9c72f137dd9de672eb2b6c
Christina Gonzalez <cmgamway@gmail.com>
2019-10-13 23:06:03 +0000

    art_throughout_the_years.rb

--- lib/art_throughout_the_years.rb
+++ lib/art_throughout_the_years.rb
@@ -1,1 +1,11 @@
+require "art_throughout_the_years/version"
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
 
+page = Nokogiri::HTML(open("https://www.metmuseum.org/toah/works/"))
+puts page.class #=> Nokogiri::HTML::Document
+
+require_relative'./art_throughout_the_years/lib/art_timeline_search'
+require_relative'./art_throughout_the_years/lib/years'
+require_relative'./art_throughout_the_years/lib/works_of_art'
