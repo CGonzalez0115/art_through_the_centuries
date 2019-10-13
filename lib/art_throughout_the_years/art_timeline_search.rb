1c9db9403f50abd030c4f62d6ef5af9d354be88e
Christina Gonzalez <cmgamway@gmail.com>
2019-10-13 18:51:46 +0000

    art_throughout_the_years/

--- lib/art_throughout_the_years/art_timeline_search.rb
+++ lib/art_throughout_the_years/art_timeline_search.rb
@@ -12,19 +12,18 @@ class ArtThroughoutTheYears :: ArtTimelineSearch
   
   def start
     puts ""
-    puts "Please type and artist, title, year, or time period you would like to search? (Please enter a selection)\n".navy_blue
-    input = gets.strip
+    puts "Please type an artist's name, title of work, year, or time period you would like to search. (Please enter your selection.)\n".navy_blue
+    input.gets.strip
     
     print_timeline(artwork)
   end
   
   def print_timeline(artwork)
     puts ""
-    puts "-------------#{artwork.name} - #{artwork.title}-------------"
-    puts "Artist Name:      #{artwork.name}"
-    puts "Title:            #{artwork.title}"
-    puts "Time Period:      #{artwork.time_period}"
-    puts "Year:             #{artwork.year}"
+    puts "---------#{artwork.name} - #{artwork.title}---------"
+    puts "Artist Name:    #{artwork.name}"
+    puts "Title:          #{artwork.title}"
+    puts "Time Period:    #{artwork.time_period}"
     puts "Please click on the object to view it's description."
   end
 end
