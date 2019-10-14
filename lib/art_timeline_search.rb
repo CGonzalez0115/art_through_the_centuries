f57c87bce253b52cbb13ae22ddf35fd62f7ab1f9
Christina Gonzalez <cmgamway@gmail.com>
2019-10-14 18:55:40 +0000

    art_timeline_search

--- lib/art_timeline_search.rb
+++ lib/art_timeline_search.rb
@@ -1,8 +1,22 @@
 class ArtTimelineSearch
+
+  attr_accessor :name, :year, :title
   ARTISTS = artist
   TITLE = title
   YEAR = []
   TIME_PERIOD = []
+  
+  @@all = []
+  def initialize(name)
+    @name = name
+    @title = title
+    @year = []
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
   
   def call
     puts "WELCOME TO YOUR ART HISTORY TIMELINE SEARCH!"
@@ -21,11 +35,38 @@ class ArtTimelineSearch
   
   def print_timeline(name, title, year, time_period)
     puts ""
-    puts "---------#{artist.name} - #{title.title}---------"
+    puts "---------#{artist.name} - #{artwork.title}---------"
     puts "Artist Name:      #{artist.name}"
-    puts "Title:            #{title.title}"
-    puts "Time Period:      #{time_period.time_period}"
+    puts "Title:            #{artwork.title}"
+    puts "Year:             #{artwork.year} - #{artist.year}"
+    puts "Time Period:      #{artwork.time_period} - #{artist.time_period}"
     puts ""
+  end
+  
+  def list_artists
+    input = gets.strip
+    if input == "list"
+      puts ""
+      artists = artist.name.all
+      artists.each_with_index(1) {|artist, index| puts "#{index}, #{artist.name}"}
+    end
+  end
+  
+  def list_artwork
+    input = gets.strip
+    if input == "list"
+      artwork = artwork.title.all
+      artwork.each_with_index(1) {|artwork, index| puts "#{index}, #{artwork.name}"}
+    end
+  end
+  
+  def list_years
+    input = gets.strip
+    if input == "list"
+      puts ""
+      years = years.name.all
+      years.each_with_index(1) {|year, index| puts "#{index}, #{year.name}"}
+    end
   end
 
 end
