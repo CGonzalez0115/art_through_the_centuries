1b1a94e0930fab746144e84bceac3c386a8644e7
Christina Gonzalez <cmgamway@gmail.com>
2019-10-14 00:49:42 +0000

    art_throughout_the_years

--- lib/art_throughout_the_years.rb
+++ lib/art_throughout_the_years.rb
@@ -3,9 +3,3 @@ require 'pry'
 require 'nokogiri'
 require 'open-uri'
 
-page = Nokogiri::HTML(open("https://www.metmuseum.org/toah/works/"))
-puts page.class #=> Nokogiri::HTML::Document
-
-require_relative'./art_throughout_the_years/lib/art_timeline_search'
-require_relative'./art_throughout_the_years/lib/years'
-require_relative'./art_throughout_the_years/lib/works_of_art'
--- lib/art_throughout_the_years/works_of_art.rb
+++ lib/art_throughout_the_years/works_of_art.rb
@@ -2,39 +2,4 @@ class Works_of_Art
   
   attr_accessor :name, :year, :title, :url, :description
   
-  @@all = []
-  def initialize
-    @name = name
-    @select_year = []
-    @title = title
-    @@all << self
-  end
-  
-  def self.all
-    @@all
-  end
-  
-  def list_artists
-    input = gets.strip
-    if input == "list"
-      puts ""
-      artists = artist.name.all
-      artists.each_with_index(1) {|artist, index| puts "#{index}, #{artist.name}"}
-    end
-  end
-  
-  def list_artwork
-    input = gets.strip
-    if input == "list"
-      artwork = artwork.title.all
-      artwork.each_with_index(1) {|artwork, index| puts "#{index}, #{artwork.name}"}
-    end
-  end
-  
-  def select_year
-    puts ""
-    puts "Please select the year or time period you would like to view.".black.bold
-    input = gets.strip
-    puts "#{works_of_art.year}"
-  end
 end
