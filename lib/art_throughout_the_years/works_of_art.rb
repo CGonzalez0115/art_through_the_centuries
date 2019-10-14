79238f343eaaf08467b3d1958c20ff5ea545c6f1
Christina Gonzalez <cmgamway@gmail.com>
2019-10-14 01:36:32 +0000

    art_throughout_the_years

--- lib/art_throughout_the_years/works_of_art.rb
+++ lib/art_throughout_the_years/works_of_art.rb
@@ -2,4 +2,32 @@ class Works_of_Art
   
   attr_accessor :name, :year, :title, :url, :description
   
+  @@all = []
+  def initialize(name)
+    @name = name
+    @title = title
+    @year = []
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def list_artists
+    input = gets.strip
+    if input == "list"
+      puts ""
+      artists = artist.name.all
+      artists.each_with_index(1) {|artist, index| puts "#{index}, #{artist.name}"}
+    end
+  end
+  
+  def list_artwork
+    input = gets.strip
+    if input == "list"
+      artwork = artwork.title.all
+      artwork.each_with_index(1) {|artwork, index| puts "#{index}, #{artwork.name}"}
+    end
+  end
 end
