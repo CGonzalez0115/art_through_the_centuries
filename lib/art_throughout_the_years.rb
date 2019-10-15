ddca3c02d778c884e2b3ddf0ddf8d7b2931b58a4
Christina Gonzalez <cmgamway@gmail.com>
2019-10-15 03:47:31 +0000

    art_throughout_years

--- lib/art_throughout_the_years.rb
+++ lib/art_throughout_the_years.rb
@@ -5,4 +5,4 @@ require 'open-uri'
 page = Nokogiri::HTML(open("https://www.metmuseum.org/toah/works/"))
 puts page.class #=> Nokogiri::HTML::Document
 
-require_relative'./art_throughout_the_years/lib/art_timeline_search'
+require_relative'./art_throughout_the_years/lib/cli'
---
+++ lib/cli.rb
@@ -0,0 +1,54 @@
+class ArtThroughoutTheYears::CLI
+  
+  def call
+    start
+  end
+
+  def list
+    puts ""
+    puts "************* The History of Art *************"
+    puts ""
+    ArtThroughoutTheYears::WorksofArt.all.each.with_index(1) do |artwork, i|
+      puts "#{i}. #{artwork.name}"
+    end
+    puts ""
+  end
+
+  def print_artwork(artwork)
+    puts ""
+    puts "-------------- #{artwork.name} --------------"
+
+    puts ""
+    puts artwork.summary
+    puts ""
+
+    puts "Creator: #{artwork.artist}"
+    puts ""
+  end
+
+  def start
+    list
+    input = nil
+    while input != "exit"
+      puts ""
+      puts "What is the name of the piece of work would you like to learn more of?"
+      puts ""
+      puts "Enter list to see the list of artwork again."
+      puts "Enter exit to end the program."
+      puts ""
+      input = gets.strip
+      if input == "list"
+        list
+      elsif input.to_i == 0
+        if artwork = ArtThroughoutTheYears::WorksofArt.find_by_name(input)
+          print_artwork(artwork)
+        end
+      elsif input.to_i > 0
+        if artwork = ArtThroughoutTheYears::WorksofArt.find(input.to_i)
+          print_artwork(artwork)
+        end
+      end
+    end
+    puts "Goodbye!!!"
+  end
+end
