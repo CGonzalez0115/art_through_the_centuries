e27a8b9e6769ea0ac9dc42ec863de89d3f4d24f3
Christina Gonzalez <cmgamway@gmail.com>
2019-10-15 05:31:07 +0000

    art_throughout_the_years

--- lib/art_throughout_the_years.rb
+++ lib/art_throughout_the_years.rb
@@ -5,8 +5,8 @@ require 'open-uri'
 page = Nokogiri::HTML(open("https://www.metmuseum.org/toah/works/"))
 puts page.class #=> Nokogiri::HTML::Document
 
-require_relative'./art_throughout_the_years/lib/cli'
-require_relative'./art_throughout_the_years/lib/works_of_art'
+require_relative "lib/cli"
+require_relative "lib/works_of_art"
 
 module ArtThroughoutTheYears
 end
