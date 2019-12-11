33b97dde07dd62ea094f3d7eb10b4f7f0d1e5cf8
Christina Gonzalez <cmgamway@gmail.com>
2019-12-11 08:34:02 +0000

    art_throughout_the_years

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,9 +5,11 @@ class ArtThroughoutTheYears::Scraper
     doc = Nokogiri::HTML(open("https://www.metmuseum.org/toah/works/"))
     doc.css('.essay-object-overlay').each do |masterpiece|
       masterpiece_details = {}
-      masterpiece_details[:name] = masterpiece.css("span.ObjArtist").text
+      masterpiece_details[:description] = masterpiece.css("div.description-wrap.accordian.openDesktop.expanded.accordian-init").text
+      masterpiece_details[:maker] = masterpiece.css("span.Maker").text
+      masterpiece_details[:artist] = masterpiece.css("span.Artist").text
       masterpiece_details[:year] = masterpiece.css("span.Date:").text
-      masterpiece_details[:title] = masterpiece.css("span.ObjTitle").text
+      masterpiece_details[:title] = masterpiece.css("h1.page-title.work-title").text
       masterpiece_details[:url] = masterpiece.css("a").attribute("href").value
       masterpieces << masterpiece_details
     end
--- lib/works_of_art.rb
+++ lib/works_of_art.rb
@@ -1,5 +1,5 @@
 class ArtThroughoutTheYears::WorksofArt
-  attr_accessor :title, :description, :artist, :year
+  attr_accessor :title, :description, :artist, :maker, :year
   
   @@ALL = []
   def initialize(title = nil)
@@ -18,30 +18,25 @@ class ArtThroughoutTheYears::WorksofArt
     self.all[id-1]
   end
 
-  def title
-    @title ||= doc.search("div[itemprop='name'] span['ObjTitle']").collect{|e| e.text.strip}.join(",")
-  end
-
   def description
-    @description ||= item_description_doc.search("p.articleBody").text.strip
+    @description ||= doc.search("div.description-wrap.accordian.openDesktop.expanded.accordian-init").text.strip
   end
 
   def artist
-    @artist ||= doc.search("div[itemprop='name'] span['ObjArtist']").text.strip
+    @artist ||= doc.search("span.Artist").text.strip
   end
   
+  def maker
+    @maker ||= doc.search("span.Maker").text.strip
+  
   def year
-    @year ||= doc.search("span[Date:]").text.strip
+    @year ||= doc.search("span.Date:").text.strip
   end
   
   def self.scrape_collection
     doc = Nokogiri::HTML(open('https://www.metmuseum.org/toah/works/'))
     names = doc.search("h1[itemprop='name'] a[itemprop='url']")
     names.collect{|e| new(e.text.strip, "https://www.metmuseum.org/toah/works/#{e.attr("href").split("?").first.strip}")}
-  end
-
-  def item_description_doc
-    @item_description_doc ||= Nokogiri::HTML(open("#{self.url}articleBody"))
   end
 
   def doc
