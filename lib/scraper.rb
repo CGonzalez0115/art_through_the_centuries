a8ef793303b0bba08c19196a35acfcd3140652fc
Christina Gonzalez <cmgamway@gmail.com>
2019-12-15 10:19:47 +0000

    art_throughout_the_years

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ class ArtThroughoutTheYears::Scraper
     doc.css('div.main_content.md-col-7.md-offset-1.xs-relative.xs-pt4.md-pt0').each do |art|
     
       index = art.css("div.FranklinGothic.number.xs-absolute.xs-text-center.cs-text-4.xs-line-height-2.xs-fill-charcoal.xs-text-white.squared.xs-px3").text
-      title_artist_year = art.css("span.title-underline.theme-title-underline-static").text
+      title_artist_year = art.css("h3.car-title.xs-text-charcoal.FranklinGothic.xs-text-2.xs-line-height-2.xs-mb4").text
       description = art.css("div.xs-text-7.xs-line-height-6.xs-text-charcoal").text
       ArtThroughoutTheYears::WorksofArt.new(index, title_artist_year, description)
     end
