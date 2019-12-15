3e1a6ac0be3183168b1b023b5cb090c74254d62d
Christina Gonzalez <cmgamway@gmail.com>
2019-12-15 09:08:10 +0000

    art_througout_the_years

--- lib/cli.rb
+++ lib/cli.rb
@@ -19,8 +19,8 @@ class ArtThroughoutTheYears::CLI
     input = gets.strip
     if input == "list"
       puts ""
-      pieces = ArtThroughoutTheYears::WorksofArt.all.collect
-      puts "#{index}. #{piece.title_artist_year}"}
+      pieces = ArtThroughoutTheYears::WorksofArt.all
+      pieces.map {|piece, index| puts "#{piece.title_artist_year}"}
     else input == "exit"
       system "clear"
       exit
