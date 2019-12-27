9e44cff0048cc118c4f9f2e701774ae69161ab0d
Christina Gonzalez <cmgamway@gmail.com>
2019-12-27 18:01:44 +0000

    art_throughout_the_years

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,49 +1,22 @@
 class ArtThroughoutTheYears::CLI
   
-  def call
-    ArtThroughoutTheYears::Scraper.scrape_art
-    start
-    list_pieces
-    print_artwork
-  end
-
-  def start
-    puts ""
-    puts "************* ~The Most Popular Paintings Throughout History~ *************"
-    puts ""
-  end
-
-  def list_pieces
-    puts "To see a list of works of art type list."
-    input = gets.strip
-    if input == "list"
-      puts ""
-      works = ArtThroughoutTheYears::WorksofArt.all.each.with_index(1) do |work, index| 
-        puts "#{index}. #{work.title_artist_year}"
-      end
-    end
-  end
-  
-  def print_artwork
-    puts ""
-    puts "Please enter the number of the listed work you would like to view, list, or Exit."
-    input = gets.strip
-    if input.to_i > 0
-      work = ArtThroughoutTheYears::WorksofArt.find(input.to_i)
-      puts ""
-      puts "Work:-------------- #{work.title_artist_year} --------------"
-      puts ""
-      puts "Information: #{work.description}"
-      puts ""
-    elsif input == "Exit"
-      puts ""
-      puts "Thank you for visiting, have a good day!"
-      puts ""
-    else
-      puts ""
-      puts "Review the list below"
-      list_pieces
-      print_artwork
-    end
-  end
+ def call
+   ArtThroughoutTheYears::Scraper.scrape_art
+   start
+   list_pieces
+   print_artwork
+ end
+ 
+ def start
+   puts ""
+   puts "-------- ~The Most Popular Paintings in History~ --------"
+   puts ""
+ end
+ 
+ def list_pieces
+   puts "To see a list of works of art type list."
+ end
+ 
+ def print_artwork
+ end
 end
