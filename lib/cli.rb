2b98cb48fbbd34d7f122947dd289d3b07b5f52e0
Christina Gonzalez <cmgamway@gmail.com>
2019-12-27 10:28:53 +0000

    art_throughout_the_years

--- lib/cli.rb
+++ lib/cli.rb
@@ -19,7 +19,7 @@ class ArtThroughoutTheYears::CLI
     if input == "list"
       puts ""
       @works = ArtThroughoutTheYears::WorksofArt.all[1]
-      @works.each.with_index(1) do |work, index| 
+      @works.each.with_index(1).join(",") do |work, index| 
         puts "#{index}. #{work.title_artist_year}"
       end
     end
