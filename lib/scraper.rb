c6a0ca64b29c6ac89da969e167a72bb05b356e8a
Christina Gonzalez <cmgamway@gmail.com>
2019-12-27 09:49:13 +0000

    art_throughout_the_years

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,7 +2,7 @@ class ArtThroughoutTheYears::Scraper
   
   def self.scrape_art
     doc = Nokogiri::HTML(open("https://www.timeout.com/newyork/art/top-famous-paintings-in-art-history-ranked"))
-    doc.css('div.main_content.md-col-7.md-offset-1.xs-relative.xs-pt4.md-pt0').each do |art|
+    doc.css('article.listCard.theme--red.category_10 .xs-mx4.md-mx0.xs-mb5.sm-pb5.sm-border-bottom.border-1.xs-full-width.xs-flex.xs-flex-column.sm-flex-row.sm-flex-wrap').each do |art|
       
       number = art.css("div.FranklinGothic.number.xs-absolute.xs-text-center.xs-z1.xs-text-4.xs-line-height-2.xs-fill-charcoal.xs-text-white.squared.xs-px3").text
       title_artist_year = art.css("h3.card-title.xs-text-charcoal.FranklinGothic.xs-text-2.xs-line-height-2.xs-mb4").text
