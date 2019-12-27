8e323ff34804e7307612f46df94df8578b8a10a5
Christina Gonzalez <cmgamway@gmail.com>
2019-12-27 09:58:28 +0000

    art_throughout_the_years

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,12 +2,12 @@ class ArtThroughoutTheYears::Scraper
   
   def self.scrape_art
     doc = Nokogiri::HTML(open("https://www.timeout.com/newyork/art/top-famous-paintings-in-art-history-ranked"))
-    doc.css('article.listCard.theme--red.category_10 .xs-mx4.md-mx0.xs-mb5.sm-pb5.sm-border-bottom.border-1.xs-full-width.xs-flex.xs-flex-column.sm-flex-row.sm-flex-wrap').each do |art|
       
-      number = art.css("div.FranklinGothic.number.xs-absolute.xs-text-center.xs-z1.xs-text-4.xs-line-height-2.xs-fill-charcoal.xs-text-white.squared.xs-px3").text
-      title_artist_year = art.css("h3.card-title.xs-text-charcoal.FranklinGothic.xs-text-2.xs-line-height-2.xs-mb4").text
-      description = art.css("div.xs-text-7.xs-line-height-6.xs-text-charcoal").text
-      ArtThroughoutTheYears::WorksofArt.new(number, title_artist_year, description)
+      work = self.new
+      work.number = doc.search("div.FranklinGothic.number.xs-absolute.xs-text-center.xs-z1.xs-text-4.xs-line-height-2.xs-fill-charcoal.xs-text-white.squared.xs-px3").text
+      work.title_artist_year = doc.search("h3.card-title.xs-text-charcoal.FranklinGothic.xs-text-2.xs-line-height-2.xs-mb4").text
+      work.description = doc.search("div.xs-text-7.xs-line-height-6.xs-text-charcoal").text
+      work
     end
   end
   
