aa7b356fc833b975043c3c08732898edfa0e680c
Christina Gonzalez <cmgamway@gmail.com>
2019-12-27 06:41:16 +0000

    art_throughout_the_years

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,7 @@ class ArtThroughoutTheYears::Scraper
     doc = Nokogiri::HTML(open("https://www.timeout.com/newyork/art/top-famous-paintings-in-art-history-ranked"))
     doc.css('div.main_content.md-col-7.md-offset-1.xs-relative.xs-pt4.md-pt0').each do |art|
     
-      title_artist_year = art.css("span.title-underline.theme-underline-static").text
+      title_artist_year = art.css("h3.card-title.xs-text-charcoal.FranklinGothic.xs-text-2.xs-line-height-2.xs-mb4").text
       description = art.css("div.xs-text-7.xs-line-height-6.xs-text-charcoal").text
       ArtThroughoutTheYears::WorksofArt.new(title_artist_year, description)
     end
