c87b4847f057e4f22f8a9dd0db222e136caa3d53
Christina Gonzalez <cmgamway@gmail.com>
2019-12-31 00:34:00 +0000

    art_throughout_the_years

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,7 +2,7 @@ class ArtThroughoutTheYears::Scraper
   
   def self.scrape_art
     doc = Nokogiri::HTML(open("https://www.timeout.com/newyork/art/top-famous-paintings-in-art-history-ranked"))
-    doc.css('#content > article > div > div > div.main_content.md-col-7.md-offset-1.xs-relative.xs-pt4.md-pt0').each do |art|
+    doc.css('div.main_content.md-col-7.md-offset-1.xs-relative.xs-pt4.md-pt0').each do |art|
       
       title = art.css("#content > article > div > div > div.main_content.md-col-7.md-offset-1.xs-relative.xs-pt4.md-pt0 > div.clearfix.xs-text-left.zone.xs-pb4.large_list.v5-zone.has-mobile-cta > div > article:nth-child(25) > div.card-content.xs-col-12 > header > h3 > span").text.strip
       description = art.css("#content > article > div > div > div.main_content.md-col-7.md-offset-1.xs-relative.xs-pt4.md-pt0 > div.clearfix.xs-text-left.zone.xs-pb4.large_list.v5-zone.has-mobile-cta > div > article:nth-child(25) > div.card-content.xs-col-12 > div.info-wrapper.xs-mb2.xs-mt2 > div > p").text.strip
