b36816c5eaecafc5ddd72048b4197d39766a3379
Christina Gonzalez <cmgamway@gmail.com>
2019-12-31 23:03:42 +0000

    art_throughout_the_years

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,12 +1,10 @@
-require 'pry'
 class ArtThroughoutTheYears::Scraper
   
   def self.scrape_art
     doc = Nokogiri::HTML(open("https://www.timeout.com/newyork/art/top-famous-paintings-in-art-history-ranked"))
     doc.css('div.main_content.md-col-7.md-offset-1.xs-relative.xs-pt4.md-pt0').each do |art|
       
-      title = art.css("h3.card-title.xs-text-charcoal.FranklinGothic.xs-text-2.xs-line-height-2.xs-mb4").text.strip.split("")
-      binding.pry
+      title = art.css("h3.card-title.xs-text-charcoal.FranklinGothic.xs-text-2.xs-line-height-2.xs-mb4").text.strip
       description = art.css("div.xs-text-7.xs-line-height-6.xs-text-charcoal").text.strip
       ArtThroughoutTheYears::WorksofArt.new(title, description)
     end
