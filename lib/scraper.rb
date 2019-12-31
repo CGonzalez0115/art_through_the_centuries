c3260733a8a8bfb2ab3c6b1a1fb427dc4e4e06ef
Christina Gonzalez <cmgamway@gmail.com>
2019-12-31 00:19:08 +0000

    art_throughout_the_years

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,8 +4,8 @@ class ArtThroughoutTheYears::Scraper
     doc = Nokogiri::HTML(open("https://www.ucityguides.com/cities/top-10-must-see-famous-works-of-art.html"))
     doc.css('main').each do |art|
       
-      title = art.css("h3").text.strip
-      description = art.css("p").text.strip
+      title = art.css("document.querySelector("#content > ol > h3:nth-child(3) > li")").text.strip
+      description = art.css("document.querySelector("#content > ol > p:nth-child(4)")").text.strip
       ArtThroughoutTheYears::WorksofArt.new(title, description)
     end
   end
