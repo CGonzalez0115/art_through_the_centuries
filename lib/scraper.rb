e6cca8c93f668d7b2aa0680679e7f66bf9c9675e
Christina Gonzalez <cmgamway@gmail.com>
2019-12-31 00:30:45 +0000

    art_throughout_the_years

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,11 +1,11 @@
 class ArtThroughoutTheYears::Scraper
   
   def self.scrape_art
-    doc = Nokogiri::HTML(open("https://www.ucityguides.com/cities/top-10-must-see-famous-works-of-art.html"))
-    doc.css('main').each do |art|
+    doc = Nokogiri::HTML(open("https://www.timeout.com/newyork/art/top-famous-paintings-in-art-history-ranked"))
+    doc.css('#content > article > div > div > div.main_content.md-col-7.md-offset-1.xs-relative.xs-pt4.md-pt0').each do |art|
       
-      title = art.css("#content > ol > h3:nth-child(3) > li").text.strip
-      description = art.css("#content > ol > p:nth-child(4)").text.strip
+      title = art.css("#content > article > div > div > div.main_content.md-col-7.md-offset-1.xs-relative.xs-pt4.md-pt0 > div.clearfix.xs-text-left.zone.xs-pb4.large_list.v5-zone.has-mobile-cta > div > article:nth-child(25) > div.card-content.xs-col-12 > header > h3 > span").text.strip
+      description = art.css("#content > article > div > div > div.main_content.md-col-7.md-offset-1.xs-relative.xs-pt4.md-pt0 > div.clearfix.xs-text-left.zone.xs-pb4.large_list.v5-zone.has-mobile-cta > div > article:nth-child(25) > div.card-content.xs-col-12 > div.info-wrapper.xs-mb2.xs-mt2 > div > p").text.strip
       ArtThroughoutTheYears::WorksofArt.new(title, description)
     end
   end
