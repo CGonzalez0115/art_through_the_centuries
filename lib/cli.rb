009b5a334924a2758913157c173099f5b0ef6d4a
Christina Gonzalez <cmgamway@gmail.com>
2020-01-01 09:03:39 +0000

    art_throughout_the_years

--- lib/cli.rb
+++ lib/cli.rb
@@ -9,23 +9,23 @@ class ArtThroughoutTheYears::CLI
 
  def start
    puts ""
-   puts "-------- ~The Most Popular Paintings in History~ --------".green
+   puts "-------- ~The Most Popular Paintings in History~ --------"bold.underline.green
    puts ""
  end
 
  def list_pieces
-   puts "To see a list of works of art type list.".blue
+   puts "To see a list of works of art type list.".bold.italic.blue
    input = gets.strip
    if input =="list"
      puts ""
      works = ArtThroughoutTheYears::WorksofArt.all.each.with_index(1) do |work, index|
-       puts "#{index}) #{work.title}".cyan
+       puts "#{index}) #{work.title}"bold.cyan
      end
    end
  end
 
  def print_artwork
-   puts "Please enter the number of the listed work you would like to view, enter list, or exit.".blue
+   puts "Please enter the number of the listed work you would like to view, enter list, or exit.".bold.italic.blue
    input = gets.strip
    if input.to_i < 24
      work = ArtThroughoutTheYears::WorksofArt.find(input.to_i)
@@ -36,14 +36,14 @@ class ArtThroughoutTheYears::CLI
      puts ""
      next_choice
     elsif input == "exit"
-     puts "Thank you for visiting!".blue
+     puts "Thank you for visiting!".bold.italic.blue
     else 
      print_artwork
     end
   end
   
   def next_choice
-    puts "Please enter another number of the listed work or enter exit to leave the program.".blue
+    puts "Please enter another number of the listed work or enter exit to leave the program.".bold.italic.blue
     input = gets.strip
     if input.to_i < 24
       work = ArtThroughoutTheYears::WorksofArt.find(input.to_i)
@@ -54,7 +54,7 @@ class ArtThroughoutTheYears::CLI
       puts ""
       next_choice
     elsif input == "exit"
-      puts "Thank you for visiting, have a nice day!".blue
+      puts "Thank you for visiting, have a nice day!".bold.italic.blue
     else
       next_choice
     end
