954cf7d1a93eab202a4b6ddb12adc18c9ce512f6
Christina Gonzalez <cmgamway@gmail.com>
2020-01-02 07:17:26 +0000

    art_throughout_the_years

--- lib/works_of_art.rb
+++ lib/works_of_art.rb
@@ -11,11 +11,7 @@ class ArtThroughoutTheYears::WorksofArt
   def self.all
     @@all
   end
-  
-  def self.new_from_index_page(r)
-    self.new(art.css("div.MFP-Home-Content-Artwork-Text").text)
-  end
-  
+
   def self.find(id)
     @@all[id-1]
   end
