c0ded55102ccb2dc744b2a8570c269d4ea4811c0
Christina Gonzalez <cmgamway@gmail.com>
2020-01-02 22:03:15 +0000

    art_throughout_the_years

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,18 +1,18 @@
 class ArtThroughoutTheYears::CLI
-  
+
   def call
     ArtThroughoutTheYears::Scraper.scrape_art
     start
     list_pieces
     print_artwork
   end
-  
+
   def start
     puts ""
     puts "-------- ~The Most Popular Paintings in History~ --------".bold.underline.green
     puts ""
   end
-  
+
   def list_pieces
     puts "To see a list of works of art type list.".bold.italic.blue
     input = gets.strip
@@ -23,33 +23,23 @@ class ArtThroughoutTheYears::CLI
       end
     end
   end
-  
+
   def print_artwork
     puts "Please enter the number of the listed work you would like to view or exit.".bold.italic.blue
     input = gets.strip
-    if input.to_i < 24
+    if input.to_i.between?(1,24)
       work = ArtThroughoutTheYears::WorksofArt.find(input.to_i)
       puts ""
       puts "Work: --#{work.title.bold.cyan}--"
       puts ""
       puts "Information: --#{work.description}--"
       puts ""
+      list_pieces
       print_artwork
+    elsif  input == "exit"
+      puts "Have a nice day!"
     else
       puts "Please enter a valid choice."
-      next_choice
-    end
-  end
-  
-  def next_choice
-    input = gets.strip
-    if input.to_i < 24
-      work = ArtThroughoutTheYears::WorksofArt.find(input.to_i)
-      puts ""
-      puts "Work: --#{work.title.bold.cyan}--"
-      puts ""
-      puts "Information: --#{work.description}--"
-      puts ""
       print_artwork
     end
   end
