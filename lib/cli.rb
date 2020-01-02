bb2911e7badd6e5f7baa04049de1504a103ceb9e
Christina Gonzalez <cmgamway@gmail.com>
2020-01-02 07:13:15 +0000

    art_throughout_the_years

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,48 +1,49 @@
 class ArtThroughoutTheYears::CLI
-
- def call
-   ArtThroughoutTheYears::Scraper.scrape_art
-   start
-   list_pieces
-   print_artwork
- end
-
- def start
-   puts ""
-   puts "-------- ~The Most Popular Paintings in History~ --------".bold.underline.green
-   puts ""
- end
-
- def list_pieces
-   puts "To see a list of works of art type list.".bold.italic.blue
-   input = gets.strip
-   if input == "list"
-     puts ""
-     works = ArtThroughoutTheYears::WorksofArt.all.each.with_index(1) do |work, index|
-       puts "#{index}) #{work.title}".cyan
-     end
-   end
- end
-
- def print_artwork
-   puts "Please enter the number of the listed work you would like to view or exit.".bold.italic.blue
-   input = gets.strip
-   if input.to_i < 24
-     work = ArtThroughoutTheYears::WorksofArt.find(input.to_i)
-    puts ""
-    puts "Work: --#{work.title.bold.cyan}--"
-    puts ""
-    puts "Information: --#{work.description}--"
-    puts ""
+  
+  def call
+    ArtThroughoutTheYears::Scraper.scrape_art
+    start
+    list_pieces
     print_artwork
-  elsif 
-    input == "exit"
+  end
+  
+  def start
     puts ""
-    puts "Thank you for visiting, have a nice day!".bold.italic.blue
+    puts "-------- ~The Most Popular Paintings in History~ --------".bold.underline.green
     puts ""
-  else 
-    puts "Please enter a valid choice."
-    puts ""
-    print_artwork
+  end
+  
+  def list_pieces
+    puts "To see a list of works of art type list.".bold.italic.blue
+    input = gets.strip
+    if input == "list"
+      puts ""
+      works = ArtThroughoutTheYears::WorksofArt.all.each.with_index(1) do |work, index|
+        puts "#{index}) #{work.title}".cyan
+      end
+    end
+  end
+  
+  def print_artwork
+    puts "Please enter the number of the listed work you would like to view or exit.".bold.italic.blue
+    input = gets.strip
+    if input.to_i < 24
+      work = ArtThroughoutTheYears::WorksofArt.find(input.to_i)
+      puts ""
+      puts "Work: --#{work.title.bold.cyan}--"
+      puts ""
+      puts "Information: --#{work.description}--"
+      puts ""
+      print_artwork
+    elsif
+      input == "exit"
+      puts ""
+      puts "Thank you for visiting, have a nice day!".bold.italic.blue
+      puts ""
+    else
+      puts "Please enter a valid choice."
+      puts ""
+      print_artwork
+    end
   end
 end
